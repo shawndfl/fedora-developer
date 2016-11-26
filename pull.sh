f96945164685ed4128a4385cef00873f0073d1ea
shawn <shawnd.android@gmail.com>
2016-11-25 23:14:05 -0500

    Add bashrc bash_profile and vimrc to user's home

--- pull.sh
+++ pull.sh
@@ -1,10 +1,23 @@
 #! /bin/bash
+
+#
+# Check for ansible
+#
 which ansible >/dev/null 2>&1
 if [ "$?" -ne "0" ]; then
    sudo yum install -y ansible python2-dnf
 fi
 
-sudo ansible-playbook local.yml
+#
+# Check for git
+#
+which git >/dev/null 2>&1
+if [ "$?" -ne "0" ]; then
+   sudo yum install -y git
+fi
+
+typeset ME=$(whoami)
+sudo ansible-playbook local.yml -v --extra-vars="user=$ME"
 
 
 
